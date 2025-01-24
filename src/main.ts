930f1a2d7b22108d9b8569d221fd8c5e420da562
LeeMing-Bou <pozzzming123@gmail.com>
2025-01-24 23:11:26 +0700

    Deploy.yml

--- src/main.ts
+++ src/main.ts
@@ -15,5 +15,6 @@ async function bootstrap() {
   // Listen to port given by environment on production server (Heroku, DigitalOcean App,..), otherwise 3000
   // Specify '0.0.0.0' in the listen() to accept connections on other hosts.
   await app.listen(process.env.PORT || 3000);
+  console.log("Listening On PORT" + " " + 3000);
 }
 bootstrap();
